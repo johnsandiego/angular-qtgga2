8598256dd0809bb338687d7529200ad9a9cb7464
johnsandiego <johnpatrick415@gmail.com>
2021-12-31 15:20:27 -0500

    1 file modified, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
@@ -18,7 +17,6 @@ import { ShippingComponent } from './shipping/shipping.component';
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartComponent },
       { path: 'shipping', component: ShippingComponent },
     ]),
   ],
@@ -28,7 +26,6 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent,
     ShippingComponent,
   ],
   bootstrap: [AppComponent],
