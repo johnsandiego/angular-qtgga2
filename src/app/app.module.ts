9ba63a5007067a9e217ab7fa04ce98b80da49e40
johnsandiego <johnpatrick415@gmail.com>
2022-01-29 17:22:08 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,8 +15,10 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent },
-  { path: 'products/:productId', component: ProductDetailsComponent},]),
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ]),
     BrowserAnimationsModule,
     MatSidenavModule,
   ],
